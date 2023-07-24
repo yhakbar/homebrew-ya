164f7426f9e24e40ebffef8868c96bc6fcc10eb1
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-07-24 10:43:03 -0400

    Bumping to `0.3.1`

--- Formula/ya.rb
+++ Formula/ya.rb
@@ -1,9 +1,9 @@
 class Ya < Formula
   desc "A very simple automation tool for lazy folks that don't want to learn make"
   homepage "https://github.com/yhakbar/ya"
-  url "https://github.com/yhakbar/ya/archive/refs/tags/0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "b786ba8c4f67ff85b70ada82bb04823bb068d206a3d047065688c8c78bc52e15"
+  url "https://github.com/yhakbar/ya/archive/refs/tags/0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "1215675fa76064cfeb0536706ee26d4f0d56ca6250cd541a02a195857da1954a"
 
   depends_on "rust" => :build
 
