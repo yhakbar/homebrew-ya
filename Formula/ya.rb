c056d7f07d71458d38d4e1f9d6d23588d0fdaecd
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-08-07 11:27:53 -0400

    Bump version to 0.6.0, including automatic installation of `yadayada`

--- Formula/ya.rb
+++ Formula/ya.rb
@@ -1,18 +1,20 @@
 class Ya < Formula
   desc "A very simple automation tool for lazy folks that don't want to learn make"
   homepage "https://github.com/yhakbar/ya"
-  url "https://github.com/yhakbar/ya/archive/refs/tags/0.5.0.tar.gz"
-  version "0.5.0"
-  sha256 "9195d70be2c937b54854f4464b69bab57bc5553a61931b32ab147959e278f4e2"
+  url "https://github.com/yhakbar/ya/archive/refs/tags/0.6.0.tar.gz"
+  version "0.6.0"
+  sha256 "0eb2615fa33c2bcfe3e118eac3834a22e76fa319362c34233234869240cf8d36"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release", "--bin", "ya"
+    system "cargo", "build", "--release", "--features", "yadayada", "--bin", "ya", "--bin", "yadayada"
     bin.install "target/release/ya"
+    bin.install "target/release/yadayada"
   end
 
   test do
     system "#{bin}/ya", "--version"
+    system "#{bin}/yadayada", "--version"
   end
 end
